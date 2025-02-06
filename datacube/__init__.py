554bd7d1faeff102f505a6385fda548b1ae67187
Damien Ayers <damien@omad.net>
2025-02-07 09:22:34 +1100

    Fix: Use canonical RTD url everywhere (#1702)
    * Fix: Use canonical RTD url everywhere
    
    * Apply suggestions from code review
    
    * Update datacube/__init__.py

--- datacube/__init__.py
+++ datacube/__init__.py
@@ -8,16 +8,9 @@ Datacube
 
 Provides access to multi-dimensional data, with a focus on Earth observations data such as LANDSAT.
 
-To use this module, see the `Developer Guide <http://datacube-core.readthedocs.io/en/stable/dev/developer.html>`_.
+To use this module, see the `Developer Guide <https://opendatacube.readthedocs.io/en/latest/installation/index.html>`_.
 
 The main class to access the datacube is :class:`datacube.Datacube`.
-
-To initialise this class, you will need a config pointing to a database, such as a file with the following::
-
-    [datacube]
-    db_hostname: 130.56.244.227
-    db_database: democube
-    db_username: cube_user
 
 """
 
