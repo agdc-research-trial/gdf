04bb8cd1150591531951c71d261ef1f240bef5de
Damien Ayers <damien@omad.net>
2025-02-17 22:18:00 +1100

    Fix dask 2024.11+ incompatibility
    Using ODC with Dask 2024.11+ made it impossible to load data, with
    failures looking like `.nodata` attribute found, or other similarly
    unhelpful messages.
    
    This was happening because the `datacube.model.Measurement` class, which
    contains important values like .dtype and .nodata, wasn't being
    correctly serialised/deserialised in those dask versions. Dask switched
    to using custom types for building the Dask Graph, but in the process
    added a special case for `dict`, which is fine, and for
    `dict-subclasses` which is more questionable. This converted all the ODC
    Measurement classes into `dict`s, which couldn't be used in the same
    way.
    
    I think it should be changed in dask, but haven't started that
    conversationn yet. For now, I've changed the Measurement class to no
    longer subclass `dict`, and instead implemented all the dunder methods
    to make it behave as a dict, while not actually being one.
    
    Replace Measurement class

--- setup.py
+++ setup.py
@@ -122,8 +122,8 @@ setup(
         'cachetools',
         'click>=5.0',
         'cloudpickle>=0.4',
-        'dask[array]<2024.11.0',  # Dask versions from 2024.11 cause problems with numpy2
-        'distributed<2024.11.0',
+        'dask[array]!=2024.11.*,!=2024.12.0',  # Dask versions >=2024.11 and < 2024.12.1 don't serialise Measurements
+        'distributed!=2024.11.*,!=2024.12.0',
         'jsonschema>=4.18',  # New reference resolution API
         'numpy>=1.26.0',
         'lark',
